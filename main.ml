4c06a17b763356eef6923da0d95886b9d4b862f0
ALICHERIF_YACINE <y.alicherif@gmail.com>
2022-03-10 20:45:42 +0100

    mon premier parseur

---
+++ main.ml
@@ -0,0 +1,11 @@
+(* fichier main.ml *)
+let _ =
+	try
+		let lexbuf = Lexing.from_channel stdin in
+		while true do
+			Parseur.main Lexeur.token lexbuf
+		done
+	with
+	| Lexeur.Eof -> exit 0
+	| Lexeur.TokenInconu
+	| Parsing.Parse_error -> Printf.printf("Ceci n'est pas une expression arithmetique\n")
