e033aa350eb3fd18b4cf6cd4516d8eea9a944987
ALICHERIF_YACINE <y.alicherif@gmail.com>
2022-03-11 10:55:58 +0100

    input

--- main.ml
+++ main.ml
@@ -1,11 +1,15 @@
 (* fichier main.ml *)
 let _ =
 	try
-		let lexbuf = Lexing.from_channel stdin in
+		let argc= Array.length Sys.argv in
+		let lexbuf = if (argc == 2) then Lexing.from_channel (open_in Sys.argv.(1)) else
+			Lexing.from_channel stdin in
 		while true do
-			Parseur.main Lexeur.token lexbuf
+			Parseur.main Lexeur.token lexbuf;
 		done
+		
 	with
 	| Lexeur.Eof -> exit 0
 	| Lexeur.TokenInconu
 	| Parsing.Parse_error -> Printf.printf("Ceci n'est pas une expression arithmetique\n")
+	
